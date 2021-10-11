ad7742158332840c7091ab9f2d322f2dabe203df
JellyBlack <jellyblack4553@gmail.com>
2021-10-11 16:52:31 +0800

    修复识别站点的问题

--- scripts/site_info.js
+++ scripts/site_info.js
@@ -3,10 +3,10 @@ hexo.extend.injector.register('body_end', `<script>
 	if(/^\\/about/.test(window.location.pathname)){
 		var info = '';
 		var hostname = window.location.hostname.toLowerCase();
-		if(hostname == 'github'){
+		if(/github\\.io$/.test(hostname)){
 			info = "您访问的站点是GitHub站点。GitHub站点是主站点，用于开发与调试，国内加载较慢，请耐心等待。国内用户建议使用Gitee站点。";
 		}
-		else if(hostname == 'gitee'){
+		else if(/gitee\\.io$/.test(hostname)){
 			info = "您访问的站点是Gitee站点。Gitee站点用于加快国内访问速度，国内用户建议使用。Gitee站点使用GitHub Actions与GitHub站点保持同步。若发现二者不同步，请叫站长重新发布一下Gitee Pages(^_−)☆";
 		}
 		else if(hostname == 'localhost' || hostname == '127.0.0.1'){
