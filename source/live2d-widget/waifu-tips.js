ff65415c24e7782d2a0627f1f1c5a171e45f02e8
JellyBlack <jellyblack4553@gmail.com>
2021-10-14 23:09:59 +0800

    移除看板娘toggle控件

--- source/live2d-widget/waifu-tips.js
+++ source/live2d-widget/waifu-tips.js
@@ -84,8 +84,7 @@ function loadWidget(config) {
 			showMessage("愿你有一天能与重要的人重逢。", 2000, 11);
 			document.getElementById("waifu").style.bottom = "-500px";
 			setTimeout(() => {
-				document.getElementById("waifu").style.display = "none";
-				document.getElementById("waifu-toggle").classList.add("waifu-toggle-active");
+				document.getElementById("waifu").style.visibility = "hidden";
 			}, 3000);
 		});
 		document.onkeydown = function(event){
@@ -263,29 +262,7 @@ function initWidget(config, apiPath) {
 			apiPath
 		};
 	}
-	document.body.insertAdjacentHTML("beforeend", `<div id="waifu-toggle">
-			<span>看板娘</span>
-		</div>`);
-	const toggle = document.getElementById("waifu-toggle");
-	toggle.addEventListener("click", () => {
-		toggle.classList.remove("waifu-toggle-active");
-		if (toggle.getAttribute("first-time")) {
-			loadWidget(config);
-			toggle.removeAttribute("first-time");
-		} else {
-			localStorage.removeItem("waifu-display");
-			document.getElementById("waifu").style.display = "";
-			setTimeout(() => {
-				document.getElementById("waifu").style.bottom = 0;
-			}, 0);
-		}
-	});
-	if (localStorage.getItem("waifu-display") && Date.now() - localStorage.getItem("waifu-display") <= 86400000) {
-		toggle.setAttribute("first-time", true);
-		setTimeout(() => {
-			toggle.classList.add("waifu-toggle-active");
-		}, 0);
-	} else {
+	if (!localStorage.getItem("waifu-display") || Date.now() - localStorage.getItem("waifu-display") > 86400000) {
 		loadWidget(config);
 	}
 }
