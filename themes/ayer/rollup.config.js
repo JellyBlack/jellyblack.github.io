43083edb0f2b3e0d1a607b74d8a6f288a8346873
JellyBlack <jellyblack4553@gmail.com>
2021-10-26 19:24:36 +0800

    格式化代码

--- themes/ayer/rollup.config.js
+++ themes/ayer/rollup.config.js
@@ -3,14 +3,14 @@ import styles from "rollup-plugin-styles";
 import { terser } from "rollup-plugin-terser";
 
 export default {
-  input: "source-src/main.js",
-  output: {
-    dir: "source/dist",
-    format: "iife",
-    assetFileNames: "[name][extname]",
-  },
-  plugins: [
-    styles({ mode: "extract", minimize: true, plugins: [autoprefixer()] }),
-    terser(),
-  ],
+    input: "source-src/main.js",
+    output: {
+        dir: "source/dist",
+        format: "iife",
+        assetFileNames: "[name][extname]",
+    },
+    plugins: [
+        styles({ mode: "extract", minimize: true, plugins: [autoprefixer()] }),
+        terser(),
+    ],
 };
